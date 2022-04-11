79ab00c88d5d8ed8e9a2e55aa01dc8dfe796c441
Aamir-Sohail <87361346+Aamir-Sohail@users.noreply.github.com>
2022-04-11 15:45:23 +0500

    first commit

--- routes/web.php
+++ routes/web.php
@@ -13,44 +13,53 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('homepage', function () {
     return view('welcome');
 });
 
 // new routes for testing;
-Route::get('/demo',function (){
-         echo "My New laravel Project";
+Route::get('/demo', function () {
+    echo "My New laravel Project";
 });
-Route::post('/test ',function (){
+Route::post('/test ', function () {
     echo "My New post laravel Project";
 });
 
-Route::put('puts /{id} ',function ($id){
+Route::put('puts /{id} ', function ($id) {
     echo "My New puts laravel Project";
 });
 
-Route::delete('user/{delete}',function ($id){
+Route::delete('user/{delete}', function ($id) {
     echo "My New  laravel Project Delete";
 });
 //use for get and post methode......Any
 // the any provide by laravel... not other sources...
-Route::any('/test ',function (){
+Route::any('/test ', function () {
     echo "My New any laravel Project";
 });
 //for new pages
-Route::get('/demo', function(){
-            return view('demo');
+Route::get('/demo', function () {
+    return view('demo');
 });
 //this routes is used for the show the specfic name /data in url also add id/
 // Route::get('/data/{name}/{id?}', function ($name,$id = null){
 //         //    return "Hello";
-//             echo $name . " "; 
+//             echo $name . " ";
 //             echo $id;
 //             // $data =compact($name,$id);
 //             // return view('demo')->with($data);
 // });
 //showing the view page data....
-Route::get('/data/{name}/{id?}',function ($name, $id = null){
-    $data =  compact('name','id');
-    return view ('demo')->with($data);
+Route::get('/data/{name}/{id?}', function ($name, $id = null) {
+    $data =  compact('name', 'id');
+    return view('demo')->with($data);
+});
+
+
+
+
+// New Project For ForntEnd.....
+Route::get('/{name?}', function ($name = null) {
+    $data = compact('name');
+    return view('home')->with($data);
 });
